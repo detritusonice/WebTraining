66ebc4e5a2fd4a46fc1a7e69dc80cf5aa50b2ef4
Thanasis Karpetis <tkarpetis@gmail.com>
2019-02-22 20:55:20 +0200

    improved waves speed, added fastwaves to play with canvas objects

---
+++ pages/fastwaves/js/fastwaves.js
@@ -0,0 +1,9 @@
+function init() {
+	createColorMap();
+	
+	var surface=document.getElementById('drawsurface');
+
+	//establish mouseclick handler
+	//create animation
+	
+}
--- pages/waves/js/waves.js
+++ pages/waves/js/waves.js
@@ -1,11 +1,12 @@
 	var sources =new Array(1); //needs a value
-	var numSources= -1;
+	var numSources= 0;
 	var startDate=new Date();
 	var startTime= startDate.getTime();//recording for delta time.
 
 	var clickable=true;
 	var dimY=40;
 	var dimX=80;
+	var shownfirst=false;
 
 function init() {
 
@@ -18,54 +19,47 @@ function init() {
 		}
 	},false);
 
-	setInterval(mapFrame,100);
+	setInterval(mapFrame,50);
 }
 
 function mapFrame() {
+	if (numSources==0 ) 
+		if (!shownfirst) shownfirst=true;
+		else return;
+
 	var d= new Date();
 	var t= d.getTime()-startTime;
 	var rate=40;// 'pixels' per period
+	var dt,front,fct,i,j,clr,s;
 
-	for (var i=0;i<dimY;i++)
-		for (var j=0;j<dimX;j++) {
-			for (var clr=0;clr<3;clr++)
+	for (i=0;i<dimY;i++)
+		for (j=0;j<dimX;j++) {
+			for (clr=0;clr<3;clr++)
 				colorMap[i][j][clr]=-1.;
 
-			for ( var s=0;s<numSources;s++) {  // cache friendlier
-				var x=sources[s][0];
-				var y=sources[s][1];
-				var dt=t-sources[s][2];
-				var dist=Math.sqrt( (x-i)*(x-i)+(y-j)*(y-j));
+			for ( s=0;s<numSources;s++) {  // cache friendlier
+				dt=t-sources[s][2];
+				front=dt-distsrc[i][j][s]*rate;
 
-				if (dt<dist*rate) continue;
-				var fct=Math.sin((dt-dist*rate)/500);
+				if (front<0.) continue;
+				fct=Math.sin(front/500.);
 				fct=fct**3;
 				for (var clr=0;clr<3;clr++)
 					colorMap[i][j][clr]+=fct*sources[s][3+clr]; 
 			}
 
-			for (var clr=0;clr<3;clr++) 
-				document.getElementById(createId(i,j)).style.backgroundColor=colorString(makeColor(colorMap[i][j][0]),
-					makeColor(colorMap[i][j][1]),makeColor(colorMap[i][j][2]));
+			pixels[i][j].style.backgroundColor= 
+				colorString(makeColor(colorMap[i][j][0]), makeColor(colorMap[i][j][1]),makeColor(colorMap[i][j][2]));
 		}
 }
 
 function makeColor( fraction ) {
-
 	fraction=Math.min(1., Math.max(-1.,fraction));
 	return Math.floor( 128+ 127*fraction);
 }
 
 function colorString( r,g,b ) {
-	var clr="#";
-	if (r<16) clr+="0";
-	clr+=r.toString(16);
-	if (g<16) clr+="0";
-	clr+=g.toString(16);
-	if (b<16) clr+="0";
-	clr+=b.toString(16);
-	return clr;
-
+	return 'rgb('+r+','+g+','+b+')';
 }
 
 function createColorMap() {
@@ -88,8 +82,13 @@ function initMap() {
 function createParticles( ) {
 	var screen=document.getElementById("screen");
 	var side=10;
+	pixels=new Array(dimY);
+	distsrc=new Array(dimY);
 
 	for ( var i=0;i<dimY; i++) {
+		pixels[i]= new Array( dimX );// one for each div element acting as pixels	
+		distsrc[i] = new Array( dimX );
+
 		var line=document.createElement("div");
 		line.id="scanline"+i.toString(10);
 		line.classList.add("scanline");
@@ -98,10 +97,10 @@ function createParticles( ) {
 		for (var j=0;j<dimX; j++) {
 			var elem=document.createElement("div");
 			elem.id=createId(i,j);
-
 			elem.classList.add("particle");
-//			elem.style.width=side.toString(10)+"px";
-//			elem.style.height=side.toString(10)+"px";
+			pixels[i][j]=elem;
+			distsrc[i][j]=new Array(1);//initially irrelevant
+			distsrc[i][j][0]=-1.;//invalid
 
 			line.appendChild(elem);
 		}
@@ -110,19 +109,26 @@ function createParticles( ) {
 }
 
 function particleClicked( element ) {
-	var x= parseInt( element.id.substr(1,2) );
-	var y= parseInt( element.id.substr(4,2) );
+	var y= parseInt( element.id.substr(1,2) );
+	var x= parseInt( element.id.substr(4,2) );
 
 	var color=Math.floor((1<<24)*Math.random());
 	var d= new Date();
 	
 	var newsource=[x,y,d.getTime()-startTime,Math.random(),Math.random(),Math.random()];//contribution to wave color
 
-	if (numSources==-1){
-		numSources=0;
+	if (numSources==0)
 		sources[0]=newsource;
-	}
-	else sources.push(newsource); 
+	else sources.push(newsource); //add the new source information
+
+	for ( var i=0;i<dimY;i++)
+		for( var j=0;j<dimX;j++)
+		{
+			if (numSources>0)
+				distsrc[i][j].push(-1.); //when numSources is 0 , an invalid element is already there
+			//for every element calculate once the distance from this source
+			distsrc[i][j][numSources]=Math.sqrt( (y-i)*(y-i)+(x-j)*(x-j)); 
+		}
 
 	numSources++;
 }
